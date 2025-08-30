9125239a90895d03ac9be5443da8a2ae7fd0b614
Maxime Maka <maxime.maka@student.hepl.be>
2025-08-30 12:12:06 +0200

    Génération d'une miniature côté Pi

--- rpi/mqtt_sub.py
+++ rpi/mqtt_sub.py
@@ -1,8 +1,29 @@
+#!/usr/bin/python3
 import configparser
 from time import localtime, strftime
 import paho.mqtt.client as mqtt
+from PIL import Image
+from io import BytesIO
 
 config = configparser.ConfigParser()
+
+def center_crop(image):
+    image.convert("RGB")
+    width, height = image.size
+    min_dimension = min(width, height)
+    left = (width - min_dimension) // 2
+    right = left + min_dimension
+    top = (height - min_dimension) // 2
+    bottom = top + min_dimension
+    return image.crop((left, top, right, bottom))
+
+def create_thumbnail(byte_image, size=(256,256)):
+    input_stream = BytesIO(byte_image)
+    output_stream = BytesIO()
+    image = Image.open(input_stream)
+    cropped = center_crop(image).resize(size, Image.LANCZOS)
+    cropped.save(output_stream, format="JPEG")
+    return output_stream.getvalue()
 
 def on_connect(client, userdata, flags, rc):
     topic = config.get('MQTT', 'Topic')
@@ -12,10 +33,20 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print('Message received')
     prefix = config.get('MQTT', 'ImageFolder')
+
+    # generate name locally (subject to change)
     name = strftime('%Y-%m-%d-%H%M%S.jpg', localtime())
-    name = prefix + '/' + name
-    with open(name, 'wb') as f:
+    image_name = prefix + '/' + name
+    thumbnail_name = prefix + '/thumb-' + name
+
+    thumbnail = create_thumbnail(msg.payload)
+
+    # currently store images locally
+    # TODO: send image and thumbnail to remote server
+    with open(image_name, 'wb') as f:
         f.write(msg.payload)
+    with open(thumbnail_name, 'wb') as f:
+        f.write(thumbnail)    
 
 def main():
 

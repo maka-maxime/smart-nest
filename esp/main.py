ec9c98c2efc9b71b1f5f73485444f23cf82b6b43
Maxime Maka <maxime.maka@student.hepl.be>
2025-08-31 22:23:59 +0200

    ESP: Publication d'un message après détection de mouvement

--- esp/main.py
+++ esp/main.py
@@ -4,7 +4,16 @@ import time
 import wlan
 import mqtt
 
+interrupted = False
+
+def sensor_interrupt(pin):
+    interrupted = True
+
+sensor = Pin(2, Pin.IN)
+sensor.irq(handler=sensor_interrupt, trigger=Pin.IRQ_RISING)
 led = Pin(33, Pin.OUT)
+flash = Pin(13, Pin.OUT)
+
 led.value(0)
 time.sleep(0.5)
 led.value(1)
@@ -16,6 +25,18 @@ config = mqtt.get_config()
 broker,port,user,node,passwd,topic = config
 client = MQTTClient(node, broker, user=user, password=passwd, port=port, ssl=False)
 client.connect()
-client.publish(topic, b'hello there')
-client.disconnect()
 
+client.publish(topic, b'HELLO')
+
+try:
+    while True:
+        if interrupted:
+            interrupted = False
+            flash.value(1)
+            client.publish(topic, b'TRIGGER')
+            flash.value(0)
+        time.sleep(1)
+except Exception as e:
+    led.value(0)
+finally:
+    client.disconnect()
